3370338f1485c3bcad9066d7059c9fa5a6684ab1
siva <shivajstme@gmail.com>
2022-09-21 05:22:32 +1000

    protected Routes

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,18 +2,33 @@ import React from "react";
 import {Home,SignIn, SignUp, Browse} from './pages';
 import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTE from './constants/routes';
+import { IsUserRedirect , ProtectedRoutes} from "./helpers/routes";
+
 export default function App() {
 
+  const user = null;
 
   return (
     <>
       <Router>
         {/* <Nav /> */}
       <Switch>
-          <Route exact path={ROUTE.HOME} component={Home} />
-          <Route  path={ROUTE.SIGN_IN} component={SignIn} />
-          <Route  path={ROUTE.SIGN_UP} component={SignUp} />
-          <Route  path={ROUTE.BROWSE} component={Browse} />
+          <IsUserRedirect user={user}  loggedInPath={ROUTE.BROWSE} path={ROUTE.SIGN_IN}>
+            <SignIn />
+          </IsUserRedirect>
+          <IsUserRedirect user={user} loggedInPath={ROUTE.BROWSE} path={ROUTE.SIGN_UP}>
+            <SignUp />
+          </IsUserRedirect>
+          {/* <Route  path={ROUTE.SIGN_UP} component={SignUp} /> */}
+          <ProtectedRoutes user={user} exact path={ROUTE.BROWSE} >
+            <Browse/>
+          </ProtectedRoutes>
+          {/* <Route  path={ROUTE.BROWSE} component={Browse} /> */}
+          <IsUserRedirect exact user= {user} loggedInPath = {ROUTE.BROWSE} path = {ROUTE.HOME} >
+            <Home/>
+          </IsUserRedirect>
+          {/* <Route  path={ROUTE.HOME} component={Home} /> */}
+
       </Switch>
     </Router>
     </>
