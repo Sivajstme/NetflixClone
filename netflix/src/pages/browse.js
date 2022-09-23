daef9203c3e5998153b85f74651ebad7c5e3958c
siva <shivajstme@gmail.com>
2022-09-23 18:35:55 +1000

    Custom hook to get the data

--- netflix/src/pages/browse.js
+++ netflix/src/pages/browse.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-
+import { useContent } from '../hooks';
 
 export default function Browse(){
 
+    const {series} = useContent('series');
+    console.log(series);
+    //Need to show the series and the films
+    // we need to show Slides Main screen
+    //
     return <p>Hello from Browse</p>
 }
